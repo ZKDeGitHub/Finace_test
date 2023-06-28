6e46342cbc07ff11f57ad81ae16b85b4278d7fb3
ZKDeGitHub <ke15290172726@gmail.com>
2023-06-28 20:12:45 +0800

    1

---
+++ src/main/java/com/javasm/controller/UserController.java
@@ -0,0 +1,12 @@
+package com.javasm.controller;
+
+/**
+ * @author: zk
+ * @className: Server
+ * @description: ToDo
+ * @date: 2023/06/28/19:56
+ * @since: 11
+ */
+public class UserController {
+
+}
--- src/main/java/com/javasm/mapper/MenuMapper.java
+++ src/main/java/com/javasm/mapper/MenuMapper.java
@@ -10,4 +10,8 @@ public interface MenuMapper {
     // 根据用户编号和权限等级查询登录用户的菜单数据
     List<Menu> queryLoginMenuList(@Param("userId") Integer userId, @Param("level") Integer level);
 
+    // 查询所有的菜单数据
+    List<Menu> queryAllMenuList(@Param("startIndex") Integer startIndex,@Param("pageSize") Integer pageSize);
+
+
 }
